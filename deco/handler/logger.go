1ce21ca450cfc6eabf4bdc03f44826bd638a2744
angryOil <jipmj13@gmail.com>
2023-10-11 01:55:38 +0900

    logger request method 표시 추가

--- deco/handler/logger.go
+++ deco/handler/logger.go
@@ -8,7 +8,7 @@ import (
 
 func Logger(w http.ResponseWriter, r *http.Request, h http.Handler) {
 	start := time.Now()
-	log.Printf("[logger] %s start", r.URL)
+	log.Printf("[%s][logger] %s start", r.Method, r.URL)
 	h.ServeHTTP(w, r)
-	log.Printf("[logger] %s elapsed time:%s\n", r.URL, time.Now().Sub(start))
+	log.Printf("[%s][logger] %s elapsed time:%s\n", r.Method, r.URL, time.Now().Sub(start))
 }
